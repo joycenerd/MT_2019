4f528273786db02184a375ee8d48287b6089475b
joycenerd <joycenerd@csie.io>
2020-01-10 22:26:55 +0800

    recognize triangle mesh, affine transformation matrix

--- final_project/affine.m
+++ final_project/affine.m
@@ -1,3 +1,17 @@
 function [atform,btform] = affine(a,b,c,cmesh)
-    
+    len=length(cmesh);
+
+    atform=zeros(3,3,len);
+    for x=1:len
+        moving=[a(cmesh(x,1),:);a(cmesh(x,2),:);a(cmesh(x,3),:)];
+        fixed=[c(cmesh(x,1),:);c(cmesh(x,2),:);c(cmesh(x,3),:)];
+        atform(:,:,x)=fitgeotrans(moving,fixed,'affine').T;
+    end
+
+    btform=zeros(3,3,len);
+    for x=1:len
+        moving=[b(cmesh(x,1),:);b(cmesh(x,2),:);b(cmesh(x,3),:)];
+        fixed=[c(cmesh(x,1),:);c(cmesh(x,2),:);c(cmesh(x,3),:)];
+        btform(:,:,x)=fitgeotrans(moving,fixed,'affine').T;
+    end
 end
--- final_project/main.m
+++ final_project/main.m
@@ -2,3 +2,4 @@ morphing1="src/coordinates/morphing1/frame0.csv";
 morphing2="src/coordinates/morphing2/frame0.csv";
 
 [a,b,c,cmesh,trinum]=mesh(morphing1,morphing2);
+[atfrom,btform]=affine(a,b,c,cmesh);
